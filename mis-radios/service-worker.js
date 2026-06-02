1130bd638a44e7543c01c367872c553a6410514f
mozotepgr-ux <mozotepgr@gmail.com>
2026-06-02 12:26:54 -0600

    Delete mis-radios directory

--- mis-radios/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", event => {
-    console.log("Service Worker instalado");
-});
-
-self.addEventListener("fetch", event => {
-    // Modo básico (sin cache avanzado por ahora)
-});
